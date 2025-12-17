51db067e63323eefb3caa3e947a849d8238f049f
Andrew Wilkes <github@urgentclick.com>
2025-12-17 10:46:52 +0900

    Update day9.go

--- day9.go
+++ day9.go
@@ -1,7 +1,5 @@
 package main
 
-// This code works with the example data but part 2 takes too long to run with the input data
-
 import (
 	"bufio"
 	"fmt"
@@ -13,6 +11,11 @@ import (
 
 type vertex struct {
 	x, y int
+}
+
+type rect struct {
+	a, b vertex
+	area int
 }
 
 func AbsInt(x int) int {
@@ -42,11 +45,13 @@ func main() {
 		vertices = append(vertices, vertex{x, y})
 	}
 
+	rects := []rect{}
 	// Part 1
 	max_area := 0
 	for i := range len(vertices) - 1 {
 		for j := i + 1; j < len(vertices); j++ {
 			a := RectArea(vertices[i], vertices[j])
+			rects = append(rects, get_rect(vertices[i], vertices[j], a))
 			if a > max_area {
 				max_area = a
 			}
@@ -56,152 +61,117 @@ func main() {
 
 	// Part 2
 
-	// The vertices form a closed path containing the green tiles.
-	// Rectangles formed by 2 vertices have 2 other corners that must be on or inside the closed path.
+	// Sort rectangles by area
+	slices.SortFunc(rects, func(x, y rect) int {
+		return y.area - x.area
+	})
 
+	// The vertices form a closed path containing the green tiles.
 	vertices = append(vertices, vertices[0])
 
-	// Loop finding the max area rectangle that has not already been rejected
-	rejected_rects := []vertex{}
-	for {
-		max_area = 0
-		var max_ids vertex
-		var v1, v2 vertex
-		for i := range len(vertices) - 1 {
-			for j := i + 1; j < len(vertices); j++ {
-				current_ids := vertex{i, j}
-				if slices.Contains(rejected_rects, current_ids) {
-					continue
-				}
-				a := RectArea(vertices[i], vertices[j])
-				if a > max_area {
-					max_area = a
-					v1 = vertices[i]
-					v2 = vertices[j]
-					max_ids = current_ids
-				}
-			}
-		}
-		// Test points along the edges of the max rect
-		ok := true
-		// Top
-		if v2.x > v1.x {
-			for x := v1.x + 1; x <= v2.x; x++ {
-				if !isInsidePolygon(vertices, vertex{x, v1.y}) {
-					ok = false
-					break
-				}
-			}
-		} else {
-			for x := v2.x; x < v1.x; x++ {
-				if !isInsidePolygon(vertices, vertex{x, v1.y}) {
-					ok = false
-					break
-				}
-			}
-		}
-		if !ok {
-			goto end
-		}
-		// Bottom
-		if v2.x > v1.x {
-			for x := v1.x; x < v2.x; x++ {
-				if !isInsidePolygon(vertices, vertex{x, v2.y}) {
-					ok = false
-					break
-				}
-			}
-		} else {
-			for x := v2.x + 1; x <= v1.x; x++ {
-				if !isInsidePolygon(vertices, vertex{x, v2.y}) {
-					ok = false
-					break
-				}
-			}
-		}
-		if !ok {
-			goto end
-		}
-		// Left
-		if v2.y > v1.y {
-			for y := v1.y + 1; y <= v2.y; y++ {
-				if !isInsidePolygon(vertices, vertex{v1.x, y}) {
-					ok = false
-					break
-				}
-			}
-		} else {
-			for y := v2.y; y < v1.y; y++ {
-				if !isInsidePolygon(vertices, vertex{v1.x, y}) {
-					ok = false
-					break
-				}
-			}
-		}
-		if !ok {
-			goto end
-		}
-		// Right
-		if v2.y > v1.y {
-			for y := v1.y; y < v2.y; y++ {
-				if !isInsidePolygon(vertices, vertex{v2.x, y}) {
-					ok = false
-					break
-				}
-			}
-		} else {
-			for y := v2.y + 1; y <= v1.y; y++ {
-				if !isInsidePolygon(vertices, vertex{v2.x, y}) {
-					ok = false
-					break
-				}
-			}
-		}
-	end:
-		if ok {
-			fmt.Println(v1, v2)
-			fmt.Printf("Part 2 max area = %d\n", max_area)
+	max_area = 0
+	start := len(rects) - 80000 // Skip some of the largest rects to save time
+	if start < 0 {
+		start = 0
+	}
+	for i := start; i < len(rects); i++ {
+		rect := rects[i]
+		if is_inside(rect, vertices) {
+			max_area = rect.area
 			break
+		}
+	}
+	fmt.Printf("Part 2 max area = %d\n", max_area)
+}
+
+func get_rect(v1, v2 vertex, area int) rect {
+	var a, b vertex
+	if v1.x < v2.x {
+		a.x = v1.x
+		b.x = v2.x
+	} else {
+		a.x = v2.x
+		b.x = v1.x
+	}
+	if v1.y < v2.y {
+		a.y = v1.y
+		b.y = v2.y
+	} else {
+		a.y = v2.y
+		b.y = v1.y
+	}
+	return rect{a, b, area}
+}
+
+func is_inside(r rect, vertices []vertex) bool {
+	// Horizontal edges
+	for x := r.a.x; x <= r.b.x; x++ {
+		if !point_inside(vertex{x, r.a.y}, vertices) {
+			return false
+		}
+		if !point_inside(vertex{x, r.b.y}, vertices) {
+			return false
+		}
+	}
+	// Vertical edges
+	for y := r.a.y + 1; y < r.b.y; y++ {
+		if !point_inside(vertex{r.a.x, y}, vertices) {
+			return false
+		}
+		if !point_inside(vertex{r.b.x, y}, vertices) {
+			return false
+		}
+	}
+	return true
+}
+
+func point_inside(p vertex, vertices []vertex) bool {
+	odd := false
+	for i := range len(vertices) - 1 {
+		a := vertices[i]
+		b := vertices[i+1]
+		// Check if point is on the line
+		if a.x == b.x {
+			// Vertical line
+			if p.x == a.x {
+				if a.y < b.y {
+					if p.y >= a.y && p.y <= b.y {
+						return true
+					}
+				} else {
+					if p.y >= b.y && p.y <= a.y {
+						return true
+					}
+				}
+			} else { // Check for crossing nodes to the left of the point
+				if a.y < b.y {
+					if a.x < p.x {
+						if a.y < p.y && b.y >= p.y {
+							odd = !odd
+						}
+					}
+				} else {
+					if a.x < p.x {
+						if b.y < p.y && a.y >= p.y {
+							odd = !odd
+						}
+					}
+				}
+			}
 		} else {
-			rejected_rects = append(rejected_rects, max_ids)
-		}
-	}
-}
-
-// Want to count how many times the point cuts though an edge vertically.
-// If the point sits on a an edge then it is inside or
-// if there is an odd number of traversals of edges up or down, then the point is inside.
-func isInsidePolygon(verts []vertex, p vertex) bool {
-	n := 0
-	m := 0
-	on_edge := false
-	last_x := -1
-	for i := range len(verts) - 1 {
-		a := verts[i]
-		b := verts[i+1]
-		if a.y == b.y { // Horizontal line
-			if a.x > b.x {
-				a = b
-				b = verts[i]
-			}
-			if a.x != last_x && p.x >= a.x && p.x <= b.x {
-				if p.y < a.y {
-					n++
-				}
-				if p.y == a.y {
-					on_edge = true
-					break
-				}
-				if p.y > a.y {
-					m++
+			// Horizontal line
+			if p.y == a.y {
+				if a.x < b.x {
+					if p.x >= a.x && p.x <= b.x {
+						return true
+					}
+				} else {
+					if p.x >= b.x && p.x <= a.x {
+						return true
+					}
 				}
 			}
-			last_x = b.x
 		}
 	}
-	return on_edge || is_odd(n) || is_odd(m)
-}
-
-func is_odd(n int) bool {
-	return n%2 != 0
+	return odd
 }
