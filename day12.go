f94046f65672a687caf2c36cee922d864c6a8063
Andrew Wilkes <github@urgentclick.com>
2025-12-31 16:47:09 +0900

    Update day12.go

--- day12.go
+++ day12.go
@@ -1,9 +1,9 @@
-package main // Incomplete
+package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
-	"slices"
 	"strconv"
 	"strings"
 )
@@ -13,7 +13,7 @@ type region struct {
 	presents      []int
 }
 
-type shape [][][]int
+type shape [][][3]int // A slice of variants of the base shape
 
 func main() {
 	file, err := os.Open("test")
@@ -31,11 +31,10 @@ func main() {
 		item := line_num % 5
 		if block < 6 {
 			if item == 0 {
-				shapes = append(shapes, shape{})
+				shapes = append(shapes, shape{[][3]int{}})
 			} else {
 				if item < 4 {
 					shapes[block][0] = append(shapes[block][0], unpack_shape_code(line))
-					set_shape_variants(shapes[block])
 				}
 			}
 		} else {
@@ -52,10 +51,15 @@ func main() {
 		}
 		line_num++
 	}
+
+	for i, sh := range shapes {
+		shapes[i] = set_shape_variants(sh)
+	}
+	fmt.Println(shapes)
 }
 
-func unpack_shape_code(line string) []int {
-	base_shape := make([]int, 3)
+func unpack_shape_code(line string) [3]int {
+	base_shape := [3]int{}
 	for i, ch := range line {
 		if ch == '#' {
 			base_shape[i] = 1
@@ -64,7 +68,8 @@ func unpack_shape_code(line string) []int {
 	return base_shape
 }
 
-func set_shape_variants(sh shape) {
+func set_shape_variants(sh shape) shape {
+	result := shape{}
 	sh = append(sh, flip_h(sh[0]))
 	sh = append(sh, flip_v(sh[0]))
 	sh = append(sh, flip_h(sh[2]))
@@ -77,56 +82,52 @@ func set_shape_variants(sh shape) {
 	for i := range len(sh) - 1 {
 		if sh[i] != nil {
 			for j := i + 1; j < len(sh); j++ {
-				if slices.Equal(sh[i][0], sh[j][0]) && slices.Equal(sh[i][1], sh[j][1]) && slices.Equal(sh[i][2], sh[j][2]) {
+				if sh[j] != nil && sh[i][0] == sh[j][0] && sh[i][1] == sh[j][1] && sh[i][2] == sh[j][2] {
 					sh[j] = nil
 				}
 			}
 		}
 	}
+	for _, s := range sh {
+		if s != nil {
+			result = append(result, s)
+		}
+	}
+	return result
 }
 
-func get_rotated_shape(a [][]int) [][]int {
-	b := make([][]int, 3)
-	for range 3 {
-		b = append(b, make([]int, 3))
-	}
-	for i := range 3 {
+func get_rotated_shape(a [][3]int) [][3]int {
+	b := make([][3]int, 3)
+	b[1][1] = a[1][1]
+	for i := range 2 {
 		b[2-i][0] = a[0][i]
 		b[0][i] = a[i][2]
 		b[i][2] = a[2][2-i]
-		b[2][i] = a[i][0]
+		b[2][2-i] = a[2-i][0]
 	}
 	return b
 }
 
-func flip_h(a [][]int) [][]int {
-	b := make([][]int, 3)
-	for range 3 {
-		b = append(b, make([]int, 3))
-	}
-	for range 3 {
-		b[0][0] = a[0][2]
-		b[1][0] = a[1][2]
-		b[2][0] = a[2][2]
-		b[0][2] = a[0][0]
-		b[1][2] = a[1][0]
-		b[2][2] = a[2][0]
-	}
+func flip_h(a [][3]int) [][3]int {
+	b := make([][3]int, 3)
+	copy(b, a)
+	b[0][0] = a[0][2]
+	b[1][0] = a[1][2]
+	b[2][0] = a[2][2]
+	b[0][2] = a[0][0]
+	b[1][2] = a[1][0]
+	b[2][2] = a[2][0]
 	return b
 }
 
-func flip_v(a [][]int) [][]int {
-	b := make([][]int, 3)
-	for range 3 {
-		b = append(b, make([]int, 3))
-	}
-	for range 3 {
-		b[0][0] = a[2][0]
-		b[0][1] = a[2][1]
-		b[0][2] = a[2][2]
-		b[2][0] = a[0][0]
-		b[2][1] = a[0][1]
-		b[2][2] = a[0][2]
-	}
+func flip_v(a [][3]int) [][3]int {
+	b := make([][3]int, 3)
+	copy(b, a)
+	b[0][0] = a[2][0]
+	b[0][1] = a[2][1]
+	b[0][2] = a[2][2]
+	b[2][0] = a[0][0]
+	b[2][1] = a[0][1]
+	b[2][2] = a[0][2]
 	return b
 }
